a2e287c48152998f4ddc0d4a511405f0176388c9
YALDABAOTH\yaldabaoth <agustin.azulay@gmail.com>
2018-04-03 11:20:53 -0400

    player movement: -moving with wasd -jumping with space -planking with shift, -rotate with e to test rotation based movement

---
+++ Assets/groundedCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundedCheck : MonoBehaviour {
+	public bool groundedDetect;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider other){
+		if (other.gameObject.name == "ground") {
+			groundedDetect = true;
+		}
+	}
+
+	public void OnTriggerExit(Collider other){
+		if (other.gameObject.name == "ground") {
+			groundedDetect = false;
+		}
+	}
+}
