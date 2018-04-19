2f8bff06bc297f7f60310bce1571f8bfc534368a
YALDABAOTH\yaldabaoth <agustin.azulay@gmail.com>
2018-04-19 11:30:14 -0400

    3 level prototypes - uti, uti2, and uti3 scenes

---
+++ Assets/buttonScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonScript : MonoBehaviour {
+
+
+	public bool present;
+	public GameObject activatable;
+	public GameObject activatable2;
+	public int playerAmt;
+	Vector3 startpos;
+
+	// Use this for initialization
+	void Start () {
+		startpos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (present) {
+			activatable.GetComponent<movingPlatformScript> ().activated = true;
+			activatable2.GetComponent<movingPlatformScript> ().activated = true;
+		} else {
+			
+			activatable.GetComponent<movingPlatformScript> ().activated = false;
+			activatable2.GetComponent<movingPlatformScript> ().activated = false;
+		}
+	}
+
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player") {
+			present = true;
+			playerAmt += 1;
+		}
+
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.tag == "Player" && present==true) {
+			if (playerAmt==1) {
+				present = false;
+			}
+			playerAmt -= 1;
+		}
+	}
+}
---
+++ Assets/movingPlatformScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatformScript : MonoBehaviour {
+
+	float yPos;
+	float startYPos;
+
+	bool movingVertically;
+	public bool activated;
+	public float distance;
+	public float moveSpeed;
+
+	Vector3 startpos;
+
+	// Use this for initialization
+	void Start () {
+		activated = false;
+		startYPos = transform.position.y;
+		startpos = transform.position;
+		yPos = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (activated) {
+
+			yPos += (distance / (Mathf.Abs (distance))) * moveSpeed;
+			if (yPos <= distance+startYPos+0.15f &&
+				yPos >= distance+startYPos-0.15f) {
+
+				yPos = distance+startYPos;
+			}
+
+
+		} else {
+			
+
+			yPos += (-(distance / Mathf.Abs (distance))) * moveSpeed;
+			if (Mathf.Abs(yPos)>=Mathf.Abs(startYPos)-0.2f 
+				&& Mathf.Abs(yPos)<=Mathf.Abs(startYPos)+0.2f){
+
+				yPos=startYPos;
+			}
+
+
+		}
+
+		transform.position = new Vector3 (transform.position.x, yPos, transform.position.z);
+	}
+}
