1ed9ba6eb307466b28c47593a6e5ad1a4e8e3fc9
Brett Moody <brettmoody@me.com>
2018-05-03 01:58:13 -0400

    Changed the level prefab to add a TV frame to all scenes.
    Created the transition scene manager scene.

---
+++ Assets/transistionSceneManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class transistionSceneManager : MonoBehaviour {
+
+	public AudioSource transistionScenceAudioSource;
+	//public AudioClip VCREntering;
+
+	float timeSinceStart = 0;
+
+	public Image panel;
+	public Text pressVToStartNewVCRTExt;
+
+
+	bool startTransition = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeSinceStart += Time.deltaTime;
+
+		if (timeSinceStart >= 5 && (int)timeSinceStart % 2 == 0) {
+			panel.enabled = true;
+			pressVToStartNewVCRTExt.enabled = true;
+		} else {
+			panel.enabled = false;
+			pressVToStartNewVCRTExt.enabled = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.V)) {
+
+			transistionScenceAudioSource.Play ();
+
+			startTransition = true;
+		}
+
+		if (startTransition && !transistionScenceAudioSource.isPlaying) 
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
