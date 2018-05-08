186eeeff62b1e540b077707362b91ef75267e0ca
Brett Moody <brettmoody@me.com>
2018-05-08 15:44:03 -0400

    Changed the VHS selection script. Changed the VHS effects.

---
+++ Assets/VHSSelectionScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class VHSSelectionScript : MonoBehaviour {
+
+	//public Button VHSSelectionButton;
+
+	public GameObject VHS;
+	bool startRotating = false;
+
+	public int levelNumber = 0;
+
+	void Update() {
+
+		if (startRotating) {
+			VHS.transform.Rotate (0, 3, 0);
+		}
+	}
+
+	public void mouseHovering () {
+		startRotating = true;
+		//Debug.Log ("VHS hovering.");
+	}
+
+	public void mouseNotHovering () {
+		startRotating = false;
+		//Debug.Log ("VHS hovering.");
+	}
+
+	public void loadScene() {
+		SceneManager.LoadScene (levelNumber, LoadSceneMode.Single);
+	}
+}
+
+
