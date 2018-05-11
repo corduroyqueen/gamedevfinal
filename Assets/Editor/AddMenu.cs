5600f447c38a1455c8003c71a3593a1e65134d43
Brett Moody <brettmoody@me.com>
2018-05-11 16:14:34 -0400

    Reset canvases in all scenes so that they fit 960 X 600.

---
+++ Assets/Editor/AddMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AddMenu : EditorWindow {
+
+	[MenuItem("Edit/Reset Playerprefs")]
+
+	public static void DeletePlayerPrefs()
+	{
+		PlayerPrefs.DeleteAll();
+	}
+
+}
